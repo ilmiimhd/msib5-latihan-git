0d761eda39dc5be0825fa615bdf0719a223b2a8e
Ilmi <139783155+ilmiimhd@users.noreply.github.com>
2023-08-23 16:54:18 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biodata</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+        <h1>Biodata Diri</h1>
+        <p>Nama: Muhammad Ilmi</p>
+        <p>MBTI: <a href="https://www.16personalities.com/intp-personality"target="_blank">INTP-T</a></p>
+        <p>Pendidikan: Institut Teknologi Telkom Purwokerto</p>
+        <p>Jurusan: Teknik Informatika</p>
+        <p>Hobi: Menonton, Fanboying aespa</p>
+        <p>Hogwards House: Ravenclaw</p>
+        <h2>Makasih gak tau lagi mau diisi apa hehe..</h2>
+        <img src="aespa.jpg" alt="aespa" class="center">
+</body>
+</html>
